ba53763a8273dae0bd7ab0277d7d1b4bbd7c96a7
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2016-12-29 11:21:58 +0100

    Add support for RFC 3339 time

--- mapper_test.go
+++ mapper_test.go
@@ -12,20 +12,25 @@ type TestRequest struct {
 	Origin          string    `query:"o"`
 	Destination     string    `query:"d"`
 	NumOfPassengers int       `query:"pax"`
-	OutwardDate     time.Time `query:"outward_date"`
-	ReturnDate      time.Time `query:"return_date,omitempty"`
+	OutwardDate     time.Time `query:"outward_date,rfc3339"`
+	ReturnDate      time.Time `query:"return_date,unix"`
 }
 
 func TestMappingStrings(t *testing.T) {
 	var r = TestRequest{}
 
-	values, _ := url.ParseQuery("o=TBW&d=LBG&pax=1&outward_date=1482852746")
-	err := mapper.Unmarshal(values, &r)
-
+	values, err := url.ParseQuery("o=TBW&d=LBG&pax=1&outward_date=1482852746&return_date=2016-12-31T11:00:00Z")
 	assert.Nil(t, err)
+
+	err = mapper.Unmarshal(values, &r)
+	assert.Nil(t, err)
+
+	rtnDate, err := time.Parse(time.RFC3339, "2016-12-31T11:00:00Z")
+	assert.Nil(t, err)
+
 	assert.Equal(t, "TBW", r.Origin)
 	assert.Equal(t, "LBG", r.Destination)
 	assert.Equal(t, time.Unix(1482852746, 0), r.OutwardDate)
-	assert.Equal(t, time.Time{}, r.ReturnDate)
+	assert.Equal(t, rtnDate, r.ReturnDate)
 	assert.Equal(t, 1, r.NumOfPassengers)
 }
